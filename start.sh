10458433e10bf7412f3bd46621312aeda3493eff
Karel Fiala <fiala.karel@gmail.com>
2015-07-15 16:54:52 +0200

    added start script for rpi

---
+++ start.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# wait for boot
+sleep 10 ;
+
+# clear log
+log="/home/pi/haut-client-start.log"
+>$log
+
+# go to 
+cd /home/pi/haut-client
+
+# update client
+git pull &>>$log
+
+# run client in screen
+screen -S 'haut-client' -d -m ./haut-client.py
+
+

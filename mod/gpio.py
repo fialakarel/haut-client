9ee9bf232b2c588ce618e999a5d5995ed750fe67
Karel Fiala <fiala.karel@gmail.com>
2015-07-17 20:27:15 +0200

    added GPIO module

---
+++ mod/gpio.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+# author: Karel Fiala
+# email: fiala.karel@gmail.com
+
+# version: 0.2.0
+
+import sys
+import RPi.GPIO as io
+io.setmode(io.BCM)
+ 
+led_pin = int(sys.argv[1])
+io.setup(led_pin, io.OUT)         # activate input
+
+if int(sys.argv[2]):
+    io.output(led_pin, True)
+else:
+    io.output(led_pin, False)
+

4855a7e94e99698ba432b4c7200d0a3a5cc1f616
Karel Fiala <fiala.karel@gmail.com>
2015-07-17 18:37:35 +0200

    real temperature module

--- mod/temperature.py
+++ mod/temperature.py
@@ -10,20 +10,30 @@ import time
 from network import Network
 from config import *
 
-# cteni argumentu
-# print(len(sys.argv))
-
-#for arg in sys.argv:
-#    print(arg)
-
-
 net = Network()
 
-while True:
-    data = dict()
-    data["temp"] = 2750
-    print(sys.argv[0] + ": " + "funguju")
-    
-    net.send(data, "main.haut.local" , SERVER_PORT)
-    
-    time.sleep(float(sys.argv[2]))
+temp_sensor = "/sys/bus/w1/devices/" + str(sys.argv[1]) + "/w1_slave"
+
+def temp_raw():
+    f = open(temp_sensor, 'r')
+    lines = f.readlines()
+    f.close()
+    return lines
+
+lines = temp_raw()
+while lines[0].strip()[-3:] != 'YES':
+    time.sleep(0.2)
+    lines = temp_raw()
+
+temp_output = lines[1].find('t=')
+
+if temp_output != -1:
+    temp_string = lines[1].strip()[temp_output+2:]
+    temp_c = float(temp_string) / 1000.0
+    return temp_c
+
+
+data = dict()
+data["key"] = "temp"
+data["value"] = temp_c
+net.send(data, "main.haut.local" , SERVER_PORT)

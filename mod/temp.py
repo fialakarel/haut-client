b44776362f03a7548624a894fe1a036aa58b9da9
Karel Fiala <fiala.karel@gmail.com>
2015-07-17 17:46:34 +0200

    added random on temp for testing

--- mod/temp.py
+++ mod/temp.py
@@ -9,6 +9,7 @@ import sys, os
 import time
 from network import Network
 from config import *
+import random
 
 # cteni argumentu
 # print(len(sys.argv))
@@ -21,7 +22,7 @@ net = Network()
 
 data = dict()
 data["key"] = "temp1"
-data["value"] = "3050"
+data["value"] = random.randint(20,30)
 #print(sys.argv[0] + ": " + "funguju")
 
 net.send(data, "main.haut.local", SERVER_PORT)
